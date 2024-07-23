bde487d11b08fd0319a24f4d57657e1ac1420a58
jhonypdev <143144945+jhonypdev@users.noreply.github.com>
2024-07-22 23:35:35 -0300

    Update Calculator.tsx

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,105 +1,113 @@
+/** @format */
+
 import React, { useContext, useState } from "react";
 import { ExchangeContext } from "../../context/ExchangeContext";
 import Container from "../Container/Container";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import Select from "../Select/Select";
-import { getExchangeRates } from "../../data/api";
 import Infos from "../Infos/Infos";
 import { TbHelp } from "react-icons/tb";
 
 const Calculator: React.FC = () => {
-  const exchangeContext = useContext(ExchangeContext);
-  if (!exchangeContext) throw new Error("ExchangeContext must be used within an ExchangeProvider");
+	const exchangeContext = useContext(ExchangeContext);
 
-  const { coins, addCalculation, showToastError, combinations } = exchangeContext;
+	if (!exchangeContext)
+		throw new Error("ExchangeContext must be used within an ExchangeProvider");
 
-  const [amount, setAmount] = useState<string>("");
-  const [fromCurrency, setFromCurrency] = useState<string>("");
-  const [toCurrency, setToCurrency] = useState<string>("");
-  const [validConversion, setValidConversion] = useState<boolean>(false);
-  const [result, setResult] = useState<number | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const { coins, convert, combinations } = exchangeContext;
 
-  const convert = async () => {
-    try {
-      await addCalculation(amount, fromCurrency, toCurrency);
-      const exchangeData = await getExchangeRates(fromCurrency, toCurrency);
-      const key = `${fromCurrency}${toCurrency}`;
-      const ask = parseFloat(exchangeData[key].ask);
-      const total = parseFloat(amount) * ask;
-      setResult(total);
-    } catch (error) {
-      showToastError(`Consulta de ${fromCurrency}-${toCurrency} não disponível.`);
-      console.error("Error fetching exchange rates:", error);
-    }
-  };
+	const [amount, setAmount] = useState<string>("");
+	const [fromCurrency, setFromCurrency] = useState<string>("");
+	const [toCurrency, setToCurrency] = useState<string>("");
+	const [validConversion, setValidConversion] = useState<boolean>(false);
+	const [result, setResult] = useState<number | null>(null);
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  const currencyOptions = Object.keys(coins).map((currency) => ({
-    value: currency,
-    label: `${currency} - ${coins[currency]}`,
-  }));
+	const handleConvert = async () => {
+		const total = await convert(amount, fromCurrency, toCurrency);
+		if (total !== null) {
+			setResult(total);
+		}
+	};
 
-  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setAmount(value);
-    const amountNumber = parseFloat(value);
-    setValidConversion(!isNaN(amountNumber) && amountNumber >= 0);
-  };
+	const currencyOptions = Object.keys(coins).map((currency) => ({
+		value: currency,
+		label: `${currency} - ${coins[currency]}`,
+	}));
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+	const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setAmount(value);
+		const amountNumber = parseFloat(value);
+		setValidConversion(!isNaN(amountNumber) && amountNumber >= 0);
+	};
 
-  return (
-    <Container className="flex flex-col gap-3 items-center md:mt-8">
-      <h2 className="text-xl text-slate-800 font-bold">Conversão Monetária</h2>
-      <div className="flex gap-2 items-center mb-4">
-        <p className="text-slate-500 m-0">Antes de seguir, veja as combinações disponíveis</p>
-        <Button variant="default" onClick={openModal} className="py-2 px-2 w-fit">
-          <TbHelp />
-        </Button>
-      </div>
-      <Input type="text" value={amount} onChange={handleAmountChange} placeholder="Montante" />
-      <Select
-        label="De"
-        options={currencyOptions}
-        value={fromCurrency}
-        onChange={(e) => setFromCurrency(e.target.value)}
-      />
-      <Select
-        label="Para"
-        options={currencyOptions}
-        value={toCurrency}
-        onChange={(e) => setToCurrency(e.target.value)}
-      />
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
 
-      <Button
-        onClick={convert}
-        className={`uppercase ${validConversion ? "" : "opacity-50 cursor-not-allowed"}`}
-        disabled={!validConversion}
-      >
-        Converter
-      </Button>
-      {result !== null && (
-        <p>
-          Resultado:{" "}
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: toCurrency,
-          }).format(result)}
-        </p>
-      )}
-      {isModalOpen && (
-        <Infos
-          lists={Object.keys(combinations).map((key) => ({
-            value: key,
-            label: `${key} - ${combinations[key]}`,
-          }))}
-          onClose={closeModal}
-        />
-      )}
-    </Container>
-  );
+	return (
+		<Container className="flex flex-col gap-3 items-center md:mt-8">
+			<h2 className="text-xl text-slate-800 font-bold">Conversão Monetária</h2>
+			<div className="flex gap-2 items-center mb-4">
+				<p className="text-slate-500 m-0">
+					Antes de seguir, veja as combinações disponíveis
+				</p>
+				<Button
+					variant="default"
+					onClick={openModal}
+					className="py-2 px-2 w-fit">
+					<TbHelp />
+				</Button>
+			</div>
+			<Input
+				type="text"
+				value={amount}
+				onChange={handleAmountChange}
+				placeholder="Montante"
+			/>
+			<Select
+				defaultValue="Selecione uma moéda"
+				label="De"
+				options={currencyOptions}
+				value={fromCurrency}
+				onChange={(e) => setFromCurrency(e.target.value)}
+			/>
+			<Select
+				label="Para"
+				options={currencyOptions}
+				value={toCurrency}
+				onChange={(e) => setToCurrency(e.target.value)}
+			/>
+
+			<Button
+				onClick={handleConvert}
+				className={`uppercase ${
+					validConversion ? "" : "opacity-50 cursor-not-allowed"
+				}`}
+				disabled={!validConversion}>
+				Converter
+			</Button>
+			{result !== null && (
+				<p>
+					Resultado:{" "}
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: toCurrency,
+					}).format(result)}
+				</p>
+			)}
+			{isModalOpen && (
+				<Infos
+					lists={Object.keys(combinations).map((key) => ({
+						value: key,
+						label: `${key} - ${combinations[key]}`,
+					}))}
+					onClose={closeModal}
+				/>
+			)}
+		</Container>
+	);
 };
 
 export default Calculator;
